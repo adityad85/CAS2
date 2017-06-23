6c8600d48b2fba1dbcd05d7381d41633deefb526
RajulShady <gamerrajul@gmail.com>
2017-06-23 08:48:21 +0530

    slight adjustments 23/6/2107

---
+++ app/src/main/java/com/rajul/cas/TeacherProfile.java
@@ -0,0 +1,73 @@
+package com.rajul.cas;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class TeacherProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    public void jumptoChangePasswordDialogTeacher(View v){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.change_password_teacher_dialog);
+        dialog.setTitle("Change Password");
+        dialog.show();
+    }
+    public void jumptoteacherprofileFromDialog(View v){
+        Intent intent = new Intent(getApplicationContext(),TeacherProfile.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_teacher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_home) {
+            Intent intent = new Intent(getApplicationContext(), HomeTeacher.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_profile) {
+            Intent intent = new Intent(getApplicationContext(), TeacherProfile.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_logout) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
